95dc1d2104a6d74a46bda2a16d544e6d99505d61
Jeffry-N <116834942+Jeffry-N@users.noreply.github.com>
2024-08-12 09:06:23 +0300

    deleted dashboard folder

--- dashboard/footer.php
+++
@@ -1,8 +0,0 @@
-<center><footer>
-        <span>Copyright © 2024 Apple Inc. All rights reserved.</span>
-        <span><a href="policy.php" class="fan">Privacy Policy</a> | </span>
-        <span><a>Terms of Use</a> | </span>
-    </footer>
-</center>
-</body>
-</html>
